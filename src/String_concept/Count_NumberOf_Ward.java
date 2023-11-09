288e0fcd940caf624b2b25ceec49d2ca78bc5204
shubham <shubham@gmail.com>
2023-11-09 10:17:56 +0530

    new

--- src/String_concept/Count_NumberOf_Ward.java
+++ src/String_concept/Count_NumberOf_Ward.java
@@ -2,7 +2,7 @@ package String_concept;
 
 public class Count_NumberOf_Ward {
 	public static void main(String[] args) {
-		String str = "pankaj sir accademy test kanha";
+		String str = "  pankaj sir accademy test kanha    ";
 		String[] s = str.trim().split(" ");
 		System.out.println(s.length);
 		for (String x : s) {
