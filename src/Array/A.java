bfce9c7bf3b18085402383529826ba06b3bbe39a
shubham <shubham@gmail.com>
2024-02-16 21:59:56 +0530

    Attay

---
+++ src/Array/A.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class A {
+	public static void main(String[] args) {
+		int[] x = {1,2,3,4,5,6,7};
+		System.out.println(x.length-1);
+		System.out.println(x.length-4);
+	}
+
+}
